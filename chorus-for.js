c7ab83c1686203f0c2084ea661a56faed89610be
teksts <mark.egelhoff1@gmail.com>
2023-04-19 11:51:56 -0700

    Cleaning and formatting

--- chorus-for.js
+++ chorus-for.js
@@ -1,5 +1,5 @@
-let chorus = "Let's dance!"
+let chorus = "Let's dance!";
 for (repeat = 0; repeat < 10; repeat++) {
-  console.log(chorus)
+  console.log(chorus);
 }
-console.log("Until the sun comes up!")
+console.log("Until the sun comes up!");
