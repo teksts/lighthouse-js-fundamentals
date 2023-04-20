2a3d80fbb98770216e25ec78145e7463e16a6be6
teksts <mark.egelhoff1@gmail.com>
2023-04-20 12:16:23 -0700

    A challenge problem for arrays

---
+++ ice-cream.js
@@ -0,0 +1,10 @@
+// Do not modify these first two lines
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+let updatedFlavours = iceCreamFlavours.concat("root beer")
+console.log(updatedFlavours)
+console.log(updatedFlavours[0])
+console.log(updatedFlavours.reverse()[0])
+console.log(updatedFlavours.length)
